d3fd9df1a16c79f32d2090d6e9e392bbc720d060
Zoe Teoh <zteoh@andrew.cmu.edu>
2019-11-20 02:28:15 -0500

    Part 2.1 replace child and task id with the name

--- app/javascript/components/Chores.js
+++ app/javascript/components/Chores.js
@@ -2,14 +2,19 @@ import React from "react"
 import PropTypes from "prop-types"
 class Chores extends React.Component {
 
-	state = { chores: []}
+	state = { 
+		chores: [],
+		tasks: []
+	}
 
 	componentDidMount() {
 		console.log("in componentDidMount")
 		this.get_chores()
+		this.get_tasks()
+		this.get_children()
 	}
 
-	run_ajax = (method, data, link, callback=function(res){return}) => {
+	run_ajax = (link, callback=function(res){return}) => {
 		console.log("in run_ajax")
 		fetch(link)
 		.then(res => res.json())
@@ -25,7 +30,37 @@ class Chores extends React.Component {
 
 	get_chores = () => {
 		console.log("in get_chores!")
-		this.run_ajax('GET', {}, '/chores.json', (res) => {this.setState({chores: res})});
+		this.run_ajax('/chores.json', (res) => {this.setState({chores: res})});
+	}
+
+	get_tasks = () => {
+        this.run_ajax('/tasks.json', (res) => {this.setState({tasks: res})});
+    }
+
+    get_children = () => {
+        this.run_ajax('/children.json', (res) => {this.setState({children: res})});
+    }
+
+	find_task_name = (chore) => {
+		var desired_id = chore.task_id;
+		const tasks = this.state.tasks
+        for (var task = 0; task < tasks.length; task += 1){
+            if (tasks[task]['id'] == desired_id){
+                return tasks[task]['name'];
+            }
+        }
+        return "No task"
+	}
+
+	find_child_name = (chore) =>{
+	    var desired_id = chore.child_id;
+	    const children = this.state.children
+	    for (var child = 0; child < children.length; child += 1){
+	        if (children[child]['id'] == desired_id){
+	            return children[child]['first_name'].concat(' ', children[child]['last_name']);
+	        }
+	    }
+	    return "No name"
 	}
 
 	displayChores = () => {
@@ -33,8 +68,8 @@ class Chores extends React.Component {
 	        // return <li key={index}>Chore ID: {value.id}</li>
 	        return (
 	        	<tr>
-		            <td width="125" align="left">{chore.child_id}</td>
-		            <td width="200" align="left">{chore.task_id}</td>
+		            <td width="125" align="left">{this.find_child_name(chore)}</td>
+		            <td width="200" align="left">{this.find_task_name(chore)}</td>
 		            <td width="75" align="center">{chore.due_on}</td>
 		            <td width="125" align="center">{chore.completed ? "True" : "False"}</td>
 		            <td></td>
